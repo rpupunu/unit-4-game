56595dfd9ab9af286240f3e1381259af71f22cc9
rpupunu <rpupunu@gmail.com>
2019-09-10 16:32:00 -0600

    Added js to generate random number and write to correcct div

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+var randomNumber
+var counter = 0;
+var totalScore = 0;
+var wins = 0;
+var loses = 0;
+
+var randomNumber = Math.floor((Math.random() * 50) + 1);
+$("#number-to-guess").text(randomNumber);
+
+
