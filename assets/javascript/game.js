f438e324eea3b06be955dea2ae2465e83e5064a9
rpupunu <rpupunu@gmail.com>
2019-09-11 16:39:18 -0600

    Finished the js, created function where I had code for var values so I could call the functions and not have to rewrite out all the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,69 @@
-$(document).ready(function() {    
-    var randomNumber
+$(document).ready(function () {
     var totalScore = 0;
     var wins = 0;
     var losses = 0;
-    var gameOver = false;
-    var crystal1
-    var crystal2
-    var crystal3
-    var crystal4
+    var randomNumber; 
+    var numberOptions;
+    var crystal1;
+    var crystal2; 
+    var crystal3; 
+    var crystal4; 
+    gameReset();
 
-    var randomNumber = Math.floor((Math.random() * 101) + 19);
+    function randomizer() {
+        randomNumber = Math.floor((Math.random() * 101) + 19);
+        numberOptions = Array.from({ length: 4 }, () => Math.floor(Math.random() * 12) + 1);
+        crystal1 = numberOptions[0];
+        crystal2 = numberOptions[1];
+        crystal3 = numberOptions[2];
+        crystal4 = numberOptions[3];  
+        console.log(crystal1, crystal2, crystal3, crystal4);
+    }
+
+    function gameReset() {
+        randomizer();
+        totalScore = 0;
+        $("#totalScore").text(totalScore);
         $("#number-to-guess").text(randomNumber);
-        console.log(randomNumber);
+    }
 
-    var numberOptions = Array.from({length: 4}, () => Math.floor(Math.random() * 12) + 1);
-        console.log(numberOptions);
-        var crystal1 = numberOptions[0];
-        var crystal2 = numberOptions[1];
-        var crystal3 = numberOptions[2];
-        var crystal4 = numberOptions[3];
+    $(".button").on("click", function () {
+        var crystalClick = $(this).attr("id");
+        console.log(crystalClick);
 
-        console.log(crystal1);
-        console.log(crystal2);
-        console.log(crystal3);
-        console.log(crystal4);
+        if (crystalClick === "crystal1") {
+            totalScore = totalScore + crystal1;
+            $("#totalScore").text(totalScore);
+        };
 
+        if (crystalClick === "crystal2") {
+            totalScore = totalScore + crystal2;
+            $("#totalScore").text(totalScore);
+        };
 
-    $(".button").on("click", function() {
-        totalScore ;
-        console.log(totalScore);
+        if (crystalClick === "crystal3") {
+            totalScore = totalScore + crystal3;
+            $("#totalScore").text(totalScore);
+        };
+
+        if (crystalClick === "crystal4") {
+            totalScore = totalScore + crystal4;
+            $("#totalScore").text(totalScore);
+        };
+
         if (totalScore === randomNumber) {
-            wins +=1;
+            wins = wins + 1;
             $("#wins").text("Wins: " + wins);
-        } 
-        else if (totalScore >= randomNumber) {
-            losses +=1;
-            $("#losses").text("Losses: " + losses);
+            gameReset();
         }
 
+        if (totalScore > randomNumber) {
+            losses = losses + 1;
+            $("#losses").text("Losses: " + losses);
+            gameReset();
+
+        }
     });
 });
-    ////////////////////////////////
+  
 
