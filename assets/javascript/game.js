2f7008f792874d653f882024f9d26aa93668ee78
rpupunu <rpupunu@gmail.com>
2019-09-10 19:26:19 -0600

    add all the css for the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var totalScore = 0;
 var wins = 0;
 var loses = 0;
 
-var randomNumber = Math.floor((Math.random() * 50) + 1);
+var randomNumber = Math.floor((Math.random() * 101) + 19);
 $("#number-to-guess").text(randomNumber);
 
 
